b10e554869a4303ea9f1e849478b17d729e6c374
Carlos Souza <carlos.pessoal@hotmail.com>
2021-03-04 15:11:22 -0300

    Adicionado cálculo de idade automático

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  const birthday = new Date("09/14/2000");
+  const monthDiff = Date.now() - birthday.getTime();
+  const ageDt = new Date(monthDiff); 
+  const year = ageDt.getUTCFullYear();
+  const age = Math.abs(year - 1970);
+  document.querySelectorAll('.info-block')[2].lastElementChild.innerHTML = `${age} anos`;
+}

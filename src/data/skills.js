39bd3baf9234df9c98152657c86b994b74dd369b
Carlos Souza <carlos.pessoal@hotmail.com>
2022-11-01 14:11:20 -0300

    refactor: with @astrojs

---
+++ src/data/skills.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    name: "HTML",
+    src: "/html.svg",
+    alt: "HTML logo",
+    color: '#F16529',
+    level: 3
+  },
+  {
+    name: "CSS",
+    src: "/css3.svg",
+    alt: "CSS logo",
+    color: '#33A9DC',
+    level: 4
+  },
+  {
+    name: "React",
+    src: "/reactjs.svg",
+    alt: "React.js logo",
+    color: '#61DAFB',
+    level: 4
+  },
+  {
+    name: "JS",
+    src: "/javascript.svg",
+    alt: "Javascript logo",
+    color: '#F7DF1E',
+    level: 4
+  },
+  {
+    name: "Node",
+    src: "/nodejs.svg",
+    alt: "Node.js logo",
+    color: '#8CC84B',
+    level: 3
+  },
+  {
+    name: "MySQL",
+    src: "/mysql.svg",
+    alt: "MySQL logo",
+    color: '#00B2F8',
+    level: 3
+  },
+  {
+    name: "Git",
+    src: "/git.svg",
+    alt: "Git logo",
+    color: '#F05133',
+    level: 3
+  },
+  {
+    name: "Docker",
+    src: "/docker.svg",
+    alt: "Docker logo",
+    color: '#2396ED',
+    level: 3
+  }
+]

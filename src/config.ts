8da76fc2506074b6e7f8dfbd44ae63696df88adb
Carlos Souza <carlos.pessoal@hotmail.com>
2022-11-04 11:04:41 -0300

    fix: title typo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Carlos Souza | Portifólio'
+export const SITE_TITLE = 'Carlos Souza | Portfólio'
 export const SITE_DESCRIPTION = 'Welcome to my website!'

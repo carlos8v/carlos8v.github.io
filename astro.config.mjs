beeabe65e9276fc80d907be98b5f238b5479e1a9
Carlos Souza <carlos.pessoal@hotmail.com>
2024-03-27 00:10:49 -0300

    fix: deploy assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://carlos8v.dev',
-  integrations: [sitemap(), tailwind()]
-});
+  integrations: [sitemap(), tailwind()],
+  build: {
+    assets: 'static',
+  },
+})

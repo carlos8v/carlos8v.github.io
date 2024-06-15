c5969ec4eca950f2dadb2d214cec0ec86578e3f7
Carlos Souza <carlos.pessoal@hotmail.com>
2024-06-15 00:37:02 -0300

    feat: add dice roller project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,6 +8,14 @@ export default [
     tags: ['crypto', 'self-hosted'],
   },
   {
+    title: 'dice-roller',
+    description: 'Simulador de dados 3D',
+    link: 'https://github.com/carlos8v/dice-roller',
+    cover:
+      'https://media.githubusercontent.com/media/carlos8v/dice-roller/main/assets/screenshot.png',
+    tags: ['3d', 'physics'],
+  },
+  {
     title: 'home-player',
     description: 'Servidor de reprodutor de música estilizado',
     link: 'https://github.com/carlos8v/home-player',
